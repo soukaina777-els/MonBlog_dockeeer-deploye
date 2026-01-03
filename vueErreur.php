fdc62196c548ad6a8922699821bd61b3e0cf486c
soukaina777-els <elouansaidisoukaina@gmail.com>
2026-01-03 03:08:19 +0000

    modife

--- vueErreur.php
+++ vueErreur.php
@@ -1,7 +1,7 @@
 <?php $titre = 'MonBlog - Erreur'; ?>
 
 <?php ob_start() ?>
-<p>Une erreur est survenue : <?= $msgErreur ?></p>
+<p>Une erreur est survenueeee : <?= $msgErreur ?></p>
 <?php $contenu = ob_get_clean(); ?>
 
 <?php require 'Gabarit.php'; ?>
